7db66128d669de39d342b819e9061ab141bceacf
koreacat <kj9966@naver.com>
2020-09-03 20:18:13 +0900

    todoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import SassComponent from "./SassComponent";
+import SassComponent from "./testComponents/SassComponent";
 
 function App() {
   return (
