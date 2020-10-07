8f27c9ba30e06a2e99fc9361c7c2ea06f14dc502
지경민 <jgm0531@midasit.com>
2020-10-07 18:01:51 +0900

    KoreanClock

--- src/component/clock/Clock.tsx
+++ src/component/clock/Clock.tsx
@@ -1,56 +1,58 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import './Clock.scss';
 import DateFormat from '../../common/DateFormat';
-import { clockProps, clockList } from './type';
+import {clockProps, clockList} from './type';
 
 
-const KoreanClock = (() => {
+const KoreanClock = (({date}: clockProps) => {
     return (
-      <div className={'koreanClock'}>
-          <table>
-              <tbody>
-                <tr>
-                    <td>낮</td>
-                    <td>다</td>
-                    <td>일</td>
-                    <td>세</td>
-                    <td>네</td>
-                </tr>
-                <tr>
-                    <td>여</td>
-                    <td>섯</td>
-                    <td>곱</td>
-                    <td>열</td>
-                    <td>한</td>
-                </tr>
-                <tr>
-                    <td>덟</td>
-                    <td>아</td>
-                    <td>홉</td>
-                    <td>두</td>
-                    <td>시</td>
-                </tr>
-                <tr>
-                    <td>밤</td>
-                    <td>정</td>
-                    <td>이</td>
-                    <td>삼</td>
-                    <td>십</td>
-                </tr>
-                <tr>
-                    <td>사</td>
-                    <td>오</td>
-                    <td>십</td>
-                    <td>오</td>
-                    <td>분</td>
-                </tr>
-              </tbody>
-          </table>
-      </div>
+        <div className={'koreanClock'}>
+            <div className={'koreanClockWrap'}>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td className={parseInt(DateFormat(date, 'HH')) >= 9 && parseInt(DateFormat(date, 'HH')) < 21 ? 'on' : ''}>낮</td>
+                        <td className={parseInt(DateFormat(date, 'hh')) == 5 ? 'on' : ''}>다</td>
+                        <td className={parseInt(DateFormat(date, 'hh')) == 7 ? 'on' : ''}>일</td>
+                        <td className={parseInt(DateFormat(date, 'hh')) == 3 ? 'on' : ''}>세</td>
+                        <td className={parseInt(DateFormat(date, 'hh')) == 4 ? 'on' : ''}>네</td>
+                    </tr>
+                    <tr>
+                        <td className={parseInt(DateFormat(date, 'hh')) == 6 || parseInt(DateFormat(date, 'hh')) == 8 ? 'on' : ''}>여</td>
+                        <td className={parseInt(DateFormat(date, 'hh')) == 5 || parseInt(DateFormat(date, 'hh')) == 6 ? 'on' : ''}>섯</td>
+                        <td className={parseInt(DateFormat(date, 'hh')) == 7 ? 'on' : ''}>곱</td>
+                        <td className={parseInt(DateFormat(date, 'hh')) >= 10 ? 'on' : ''}>열</td>
+                        <td className={parseInt(DateFormat(date, 'hh')) == 1 || parseInt(DateFormat(date, 'hh')) == 11 ? 'on' : ''}>한</td>
+                    </tr>
+                    <tr>
+                        <td className={parseInt(DateFormat(date, 'hh')) == 8 ? 'on' : ''}>덟</td>
+                        <td className={parseInt(DateFormat(date, 'hh')) == 9 ? 'on' : ''}>아</td>
+                        <td className={parseInt(DateFormat(date, 'hh')) == 9 ? 'on' : ''}>홉</td>
+                        <td className={parseInt(DateFormat(date, 'hh')) == 2 || parseInt(DateFormat(date, 'hh')) == 12 ? 'on' : ''}>두</td>
+                        <td className={'on'}>시</td>
+                    </tr>
+                    <tr>
+                        <td className={parseInt(DateFormat(date, 'HH')) >= 21 || parseInt(DateFormat(date, 'HH')) < 9 ? 'on' : ''}>밤</td>
+                        <td className={parseInt(DateFormat(date, 'HH')) == 12 ? 'on' : ''}>정</td>
+                        <td className={parseInt(DateFormat(date, 'mm')) >= 20 && parseInt(DateFormat(date, 'mm')) < 30 ? 'on' : ''}>이</td>
+                        <td className={parseInt(DateFormat(date, 'mm')) >= 30 && parseInt(DateFormat(date, 'mm')) < 40 ? 'on' : ''}>삼</td>
+                        <td className={parseInt(DateFormat(date, 'mm')) >= 10 && parseInt(DateFormat(date, 'mm')) < 20 ? 'on' : ''}>십</td>
+                    </tr>
+                    <tr>
+                        <td className={parseInt(DateFormat(date, 'mm')) >= 40 && parseInt(DateFormat(date, 'mm')) < 50 ? 'on' : ''}>사</td>
+                        <td className={parseInt(DateFormat(date, 'mm')) >= 50 || parseInt(DateFormat(date, 'HH')) == 12 ? 'on' : ''}>오</td>
+                        <td className={parseInt(DateFormat(date, 'mm')) >= 10 ? 'on' : ''}>십</td>
+                        <td className={parseInt(DateFormat(date, 'mm')) % 10 >= 5 ? 'on' : ''}>오</td>
+                        <td className={parseInt(DateFormat(date, 'mm')) > 5 ? 'on' : ''}>분</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     );
 });
 
-const DigitalClock = ({ date }: clockProps) => {
+const DigitalClock = ({date}: clockProps) => {
     return (
         <div className={'digitalClock'}>
             <p>
@@ -61,7 +63,7 @@ const DigitalClock = ({ date }: clockProps) => {
     )
 };
 
-const AnalogClock = ({ date }: clockProps) => {
+const AnalogClock = ({date}: clockProps) => {
     const h_s = date.getHours() * 60 * 60;
     const m_s = date.getMinutes() * 60;
     const s = date.getSeconds();
@@ -91,7 +93,7 @@ const AnalogClock = ({ date }: clockProps) => {
     );
 };
 
-const DigilogClocks = (({ date, digilogClockList }: any) => {
+const DigilogClocks = (({date, digilogClockList}: any) => {
     const ns = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     const h1 = Math.floor(date.getHours() / 10);
     const h2 = date.getHours() % 10;
@@ -102,45 +104,53 @@ const DigilogClocks = (({ date, digilogClockList }: any) => {
 
     return (
         <div className={'digilogClocks'}>
-            <table>
-                <tbody>
+            <div className={'digilogClockWrap'}>
+                <table>
+                    <tbody>
                     {
-                     digilogClockList.map((digilogClocks: any, index: number) => (
-                     <tr key={index}>
-                         {
-                             digilogClocks.map((clock: string, index: number) =>
-                                <td key={index}>
-                                    <div className={
-                                        'digilogClock ' + 
-                                        clock.replace(/(h1|h2|m1|m2|s1|s2)/gi, ((__: any) => {
-                                            switch (__) {
-                                                case 'h1': return ns[h1];
-                                                case 'h2': return ns[h2];
-                                                case 'm1': return ns[m1];
-                                                case 'm2': return ns[m2];
-                                                case 's1': return ns[s1];
-                                                case 's2': return ns[s2];
-                                                default: return __;
-                                            }
-                                        }))
-                                        }>
-                                        <div className={'h'}></div>
-                                        <div className={'m'}></div>
-                                     </div>
-                                </td>
-                             )
-                         }
-                     </tr>))
+                        digilogClockList.map((digilogClocks: any, index: number) => (
+                            <tr key={index}>
+                                {
+                                    digilogClocks.map((clock: string, index: number) =>
+                                        <td key={index}>
+                                            <div className={
+                                                'digilogClock ' +
+                                                clock.replace(/(h1|h2|m1|m2|s1|s2)/gi, ((__: any) => {
+                                                    switch (__) {
+                                                        case 'h1':
+                                                            return ns[h1];
+                                                        case 'h2':
+                                                            return ns[h2];
+                                                        case 'm1':
+                                                            return ns[m1];
+                                                        case 'm2':
+                                                            return ns[m2];
+                                                        case 's1':
+                                                            return ns[s1];
+                                                        case 's2':
+                                                            return ns[s2];
+                                                        default:
+                                                            return __;
+                                                    }
+                                                }))
+                                            }>
+                                                <div className={'h'}></div>
+                                                <div className={'m'}></div>
+                                            </div>
+                                        </td>
+                                    )
+                                }
+                            </tr>))
                     }
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 });
 
 const Clock = () => {
     const [date, setDate] = useState(new Date());
-    const [digilog, setDigilog] = useState(true);
     const [digilogClockList, setDigilogClockList] = useState(clockList);
 
     useEffect(() => {
@@ -153,20 +163,13 @@ const Clock = () => {
         }
     }, []);
 
-    const clockClick = useCallback(() => {
-        setDigilog(!digilog);
-    }, [digilog]);
 
     return (
-        <div className={'clock'} onClick={clockClick}>
-            {!digilog ?
-                <>
-                    <AnalogClock date={date} />
-                    <DigitalClock date={date} />
-                </>
-                :
-                <DigilogClocks date={date} digilogClockList={digilogClockList} />
-            }
+        <div className={'clock'}>
+            <AnalogClock date={date}/>
+            <DigitalClock date={date}/>
+            <DigilogClocks date={date} digilogClockList={digilogClockList}/>
+            <KoreanClock date={date}/>
         </div>
     );
 };
