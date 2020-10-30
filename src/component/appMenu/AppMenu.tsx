cc254cb56a505cefa81d4df32ba8095ce9c95659
지경민 <jgm0531@midasit.com>
2020-10-30 10:26:07 +0900

    AppMenu 가로 스크롤 기능 추가

--- src/component/appMenu/AppMenu.tsx
+++ src/component/appMenu/AppMenu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './AppMenu.scss';
 import Clock from '../clock/Clock';
 import HamderTale from '../hamderTale/HamderTale';
@@ -7,11 +7,11 @@ import HampleStory from '../hampleStory/HampleStory';
 import HamTaker from '../hamTaker/HamTaker';
 import Canvas from '../canvas/App';
 
-const AppIcon = ({title, img, onClick}:any) => {
+const AppIcon = ({title, img, onClick}: any) => {
     const style = {
-        backgroundImage: `url(${require(img+'')})`
+        backgroundImage: `url(${require(img + '')})`
     };
-    return(
+    return (
         <div className={'appIcon'} onClick={onClick} style={style}>
             <span className={'appIconName'}>{title}</span>
         </div>
@@ -19,14 +19,52 @@ const AppIcon = ({title, img, onClick}:any) => {
 };
 
 const AppMenu = ({setAppState}: any) => {
-    return(
-        <div className={'appMenu'}>
-            <AppIcon title={'clock'} img={'./img/clock_icon.jpg'} onClick={() => {setAppState(<Clock />)}}/>
-            <AppIcon title={'hamderTale'} img={'./img/hamderTale_icon.png'} onClick={() => {setAppState(<HamderTale />)}}/>
-            <AppIcon title={'hamKemon'} img={'./img/hamKemon_icon.png'} onClick={() => {setAppState(<HamKemon />)}}/>
-            <AppIcon title={'hampleStory'} img={'./img/hampleStory_icon.png'} onClick={() => {setAppState(<HampleStory />)}}/>
-            <AppIcon title={'hamTaker'} img={'./img/hamTaker_icon.jpg'} onClick={() => {setAppState(<HamTaker />)}}/>
-            <AppIcon title={'canvas'} img={'./img/canvas_icon.png'} onClick={() => {setAppState(<Canvas />)}}/>
+    const appMenu: any = useRef(null);
+
+    const scrollEvent = (e: any) => {
+        let diff = e.deltaY;
+        window.requestAnimationFrame(() => {
+            appMenu.current.scrollLeft += diff;
+        });
+    };
+
+    return (
+        <div ref={appMenu} className={'appMenu'} onWheel={scrollEvent}>
+            <AppIcon title={'clock'} img={'./img/clock_icon.jpg'} onClick={() => {setAppState(<Clock/>)}}/>
+            <AppIcon title={'hamderTale'} img={'./img/hamderTale_icon.png'} onClick={() => {setAppState(<HamderTale/>)}}/>
+            <AppIcon title={'hamKemon'} img={'./img/hamKemon_icon.png'} onClick={() => {setAppState(<HamKemon/>)}}/>
+            <AppIcon title={'hampleStory'} img={'./img/hampleStory_icon.png'} onClick={() => {setAppState(<HampleStory/>)}}/>
+            <AppIcon title={'hamTaker'} img={'./img/hamTaker_icon.jpg'} onClick={() => {setAppState(<HamTaker/>)}}/>
+            <AppIcon title={'canvas'} img={'./img/canvas_icon.png'} onClick={() => {setAppState(<Canvas/>)}}/>
+            <AppIcon title={'drive'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'parrot'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'cube'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'artGalley'} img={'./img/default_icon.png'} onClick={() => {}}/>
+
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
+            <AppIcon title={'dummy'} img={'./img/default_icon.png'} onClick={() => {}}/>
         </div>
     );
 };
