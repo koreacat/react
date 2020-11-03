d065d357e5378586269de17f75315eb571bcbd0a
김형규 <khg0712@midasit.com>
2020-11-03 12:13:01 +0900

    canvas mock 모듈 추가

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import 'jest-canvas-mock'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+	test("renders well", () => {
+		expect(() => render(<App />)).not.toThrowError();
+	});
 });
