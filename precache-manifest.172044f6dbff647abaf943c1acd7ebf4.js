515be5a7ca34808b32fb5ead6856e2e43bb6d4ba
koreacat <kj9966@naver.com>
2020-11-12 23:44:25 +0900

    Updates

---
+++ precache-manifest.172044f6dbff647abaf943c1acd7ebf4.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda799c4af5774f6b819ad8ac61d4b45",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "480411e3f38782c2cf17",
+    "url": "/react/static/css/main.75d2afd4.chunk.css"
+  },
+  {
+    "revision": "d6f32013308530a8fcf8",
+    "url": "/react/static/js/2.0d81f44c.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/react/static/js/2.0d81f44c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "480411e3f38782c2cf17",
+    "url": "/react/static/js/main.e80f158c.chunk.js"
+  },
+  {
+    "revision": "790d185f1e3865afd495",
+    "url": "/react/static/js/runtime-main.0a4db6bd.js"
+  },
+  {
+    "revision": "a13b9e3eca58f113343bf6d6b842ac45",
+    "url": "/react/static/media/001.a13b9e3e.jpg"
+  },
+  {
+    "revision": "1d2d2b3fff5cd9c8290da9b23011d069",
+    "url": "/react/static/media/004.1d2d2b3f.jpg"
+  },
+  {
+    "revision": "a3ac4c0bec7fa60303c5affc31c4df40",
+    "url": "/react/static/media/006.a3ac4c0b.jpg"
+  },
+  {
+    "revision": "dfc1fed8e6f875a8d096230eaa1fafb9",
+    "url": "/react/static/media/009.dfc1fed8.jpg"
+  },
+  {
+    "revision": "681e5e2d90105df779b5e4876513b450",
+    "url": "/react/static/media/011.681e5e2d.jpg"
+  },
+  {
+    "revision": "2a1537efe4e635eacf3f05ab35ec03bc",
+    "url": "/react/static/media/015.2a1537ef.jpg"
+  },
+  {
+    "revision": "5d4d2b9279b3809bec48765872c25850",
+    "url": "/react/static/media/017.5d4d2b92.jpg"
+  },
+  {
+    "revision": "876b18df87d68e90c4ba75df94e44e51",
+    "url": "/react/static/media/035.876b18df.jpg"
+  },
+  {
+    "revision": "17b348b991064ff3125b75cddb97bf77",
+    "url": "/react/static/media/046.17b348b9.jpg"
+  },
+  {
+    "revision": "147fb1532a5e7cd74ea39a0bcae29977",
+    "url": "/react/static/media/055.147fb153.jpg"
+  },
+  {
+    "revision": "3efa411a53b79fe3777b077d9972ccf1",
+    "url": "/react/static/media/Buttercup.3efa411a.mp3"
+  },
+  {
+    "revision": "6deb7b5bfcb57c198ae7e9006db3ca27",
+    "url": "/react/static/media/clock_icon.6deb7b5b.jpg"
+  },
+  {
+    "revision": "fcc53b08127a4667154c72488ec0f46f",
+    "url": "/react/static/media/gasterBlaster.fcc53b08.mp3"
+  },
+  {
+    "revision": "65c4e7dac52ae7239b229bfd6f989290",
+    "url": "/react/static/media/gasterClose.65c4e7da.png"
+  },
+  {
+    "revision": "29ca9d9f0794bcaf065d7e6e65ede77e",
+    "url": "/react/static/media/gasterOpen.29ca9d9f.png"
+  },
+  {
+    "revision": "a98989af613e325329e128e09eefcc3a",
+    "url": "/react/static/media/hamTakerDone.a98989af.gif"
+  },
+  {
+    "revision": "263ec213223395ccea02c70f7e4ee69d",
+    "url": "/react/static/media/hampleStory_icon.263ec213.png"
+  },
+  {
+    "revision": "219aa90f9dffb758c900f7604f07828f",
+    "url": "/react/static/media/hurt.219aa90f.wav"
+  },
+  {
+    "revision": "daf510e64b324d2a3861cd8512749dd4",
+    "url": "/react/static/media/move.daf510e6.wav"
+  },
+  {
+    "revision": "493a8d3ed9cffe7750c070788210a13c",
+    "url": "/react/static/media/redo.493a8d3e.svg"
+  },
+  {
+    "revision": "719811307d52635ba1ee8369527c4db6",
+    "url": "/react/static/media/sans.71981130.gif"
+  },
+  {
+    "revision": "91c4c92969dd4ed1d80f8b8272e041ec",
+    "url": "/react/static/media/sans.91c4c929.wav"
+  },
+  {
+    "revision": "5b452b1de7e50188c8cd9595233ee29c",
+    "url": "/react/static/media/screen_changer_part1.5b452b1d.wav"
+  },
+  {
+    "revision": "552a3de4022e8536c35a744188897c63",
+    "url": "/react/static/media/screen_changer_part2.552a3de4.wav"
+  },
+  {
+    "revision": "624d504fcfcd131b57ffda1eb04cb1d9",
+    "url": "/react/static/media/spikes.624d504f.wav"
+  },
+  {
+    "revision": "f996511371f4d0fb6d460ad17e947fa6",
+    "url": "/react/static/media/stone_kick.f9965113.wav"
+  },
+  {
+    "revision": "32618059412ca8671a2930871039f86f",
+    "url": "/react/static/media/success.32618059.wav"
+  },
+  {
+    "revision": "7d953db9a9510a12f6ea0657304859f2",
+    "url": "/react/static/media/undo.7d953db9.svg"
+  },
+  {
+    "revision": "bb6436ece89207e72dc56ed0b9f71efd",
+    "url": "/react/static/media/루시퍼.bb6436ec.gif"
+  },
+  {
+    "revision": "d08b3d7bdb182910ed54874fb52e825e",
+    "url": "/react/static/media/말리나.d08b3d7b.gif"
+  },
+  {
+    "revision": "38aadb774ee919de21c377676f9a91a1",
+    "url": "/react/static/media/모데우스.38aadb77.gif"
+  },
+  {
+    "revision": "4a712d7440063eb4394b9cbdf235c02e",
+    "url": "/react/static/media/아자젤.4a712d74.gif"
+  },
+  {
+    "revision": "cfad3877aaab0cb2e843a11fcd748fe6",
+    "url": "/react/static/media/저스티스.cfad3877.gif"
+  },
+  {
+    "revision": "ba4d847b08ed457038cd61b70c240a6a",
+    "url": "/react/static/media/저지먼트.ba4d847b.gif"
+  },
+  {
+    "revision": "7eda489d6f10bb30ce81b6bb2c638273",
+    "url": "/react/static/media/즈드라다.7eda489d.gif"
+  },
+  {
+    "revision": "5954e79f34625e9ffd1f534f609a50e9",
+    "url": "/react/static/media/케르베로스.5954e79f.gif"
+  },
+  {
+    "revision": "66896bfe0f8c61e0a58a19d6bd57bf44",
+    "url": "/react/static/media/판데모니카.66896bfe.gif"
+  },
+  {
+    "revision": "82f1c0370fd42c26667e4b7ae7dde35c",
+    "url": "/react/static/media/해골.82f1c037.gif"
+  },
+  {
+    "revision": "f1013f24bdd6140605cf746eff1f4df3",
+    "url": "/react/static/media/헬테이커.f1013f24.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98a17999df94e78e591d915422d862a6.js"
+  "/react/precache-manifest.172044f6dbff647abaf943c1acd7ebf4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
