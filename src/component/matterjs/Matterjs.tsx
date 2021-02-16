67c2aec9e03fb62cf33af999cf53ef483d839904
지경민 <jgm0531@midasit.com>
2021-02-16 17:01:17 +0900

    MatterJS

--- src/component/matterjs/Matterjs.tsx
+++ src/component/matterjs/Matterjs.tsx
@@ -1,39 +1,47 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect} from 'react';
 import './Matterjs.scss';
 import Matter from 'matter-js';
 
-const Matterjs = () => {
-	useEffect(() => {
-		const { Engine, Render, Bodies, World } = Matter;
-		let engine = Engine.create();
-		let canvas = document.getElementById('matterjsCanvas');
+const { Engine, Render, Bodies, World } = Matter;
+const engine = Engine.create();
+let canvas: any = null;
+let render: any = null;
 
-		// @ts-ignore
-		let render = Render.create({element: canvas, engine: engine, options: {
-				width: '600',
-				height: '500',
-			}
-		});
+const init = () => {
+	canvas = document.getElementById('matterjsCanvas');
+	while (canvas.hasChildNodes()) canvas.removeChild(canvas.firstChild);
+	render = Render.create({element: canvas, engine: engine, options: {
+			width: 600,
+			height: 500,
+		}
+	});
+};
 
-		let ground = Bodies.rectangle(300, 480, 600, 10, {isStatic: true});
-		let wallA = Bodies.rectangle(10, 250, 10, 500, {isStatic: true});
-		let wallB = Bodies.rectangle(590, 250, 10, 500, {isStatic: true});
+const draw = () => {
+	let ground = Bodies.rectangle(300, 480, 600, 10, {isStatic: true});
+	let wallA = Bodies.rectangle(10, 250, 10, 500, {isStatic: true});
+	let wallB = Bodies.rectangle(590, 250, 10, 500, {isStatic: true});
 
-		let circleA = Bodies.circle(400, 200, 40, {
-			// friction: 0.001,
-			// frictionAir: 0.01,
-			restitution: 0.9,
-		});
-		let circleB = Bodies.circle(450, 50, 60, {
-			restitution: 0.9,
-		});
-
-		World.add(engine.world, [circleA, circleB, ground, wallA, wallB]);
-		Engine.run(engine);
-		Render.run(render);
+	let circleA = Bodies.circle(400, 200, 40, {
+		// friction: 0.001,
+		// frictionAir: 0.01,
+		restitution: 0.9,
+	});
+	let circleB = Bodies.circle(450, 50, 60, {
+		restitution: 0.9,
 	});
 
+	World.add(engine.world, [circleA, circleB, ground, wallA, wallB]);
+	Engine.run(engine);
+	Render.run(render);
+};
 
+
+const Matterjs = () => {
+	useEffect(() => {
+		init();
+		draw();
+	});
 
 	return (
 		<div className={'matterjs'}>
